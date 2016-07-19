461b4a3a2ef72e34172a35ad9c306d239d77e849
lsl <li_senlin@foxmail.com>
2016-07-19 09:54:13 +0800

    更新
    群发和附件

--- easymail/src/cn/yuhi/util/MailUtil.java
+++ easymail/src/cn/yuhi/util/MailUtil.java
@@ -1,13 +1,19 @@
 package cn.yuhi.util;
 
+import java.util.List;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import cn.yuhi.dto.MimeMessageDTO;
 
@@ -40,36 +46,83 @@ public class MailUtil {
 	 */   
 	private String targetAddress;
 	
-	/** 
-	 * 方法名: sendEmail 
-	 * 功能描述: TODO 发送邮件
-	 * @param: @param userName 邮箱账号
-	 * @param: @param password 邮箱密码
-	 * @param: @param targetAddress 目标邮箱地址
-	 * @param: @param mimeDTO 邮件部分参数
-	 * @return: boolean 
+	/**
+	 *  发送单邮件
+	 * @param userName
+	 * @param password
+	 * @param targetAddress
+	 * @param mimeDTO
+	 * @return
 	 */
-	public static boolean sendEmail(String userName,String password,String targetAddress,MimeMessageDTO mimeDTO){
-		Properties props = new Properties();
+	public  static boolean sendEmail(String userName,String password,String targetAddress,
+			MimeMessageDTO mimeDTO){
+		return publicsendEmail(userName,password,targetAddress,mimeDTO,false,null);
+	}
+	/**
+	 * 发送单邮件(附件)
+	 * @param userName
+	 * @param password
+	 * @param targetAddress
+	 * @param mimeDTO
+	 * @param filepath        文件本地绝对路径
+	 * @return
+	 */
+	public  static boolean sendEmailByFile(String userName,String password,String targetAddress,
+			MimeMessageDTO mimeDTO,List<String> filepath){
+		return publicsendEmail(userName,password,targetAddress,mimeDTO,true,filepath);
+	}
+	/**
+	 * 群发邮件 
+	 * @param userName
+	 * @param password
+	 * @param targetAddress   多个邮件发送地址，以,分隔
+	 * @param mimeDTO
+	 * @return
+	 */
+	public  static boolean sendGroupEmail(String userName,String password,String targetAddress,
+			MimeMessageDTO mimeDTO){
+		return publicsendEmail(userName,password,targetAddress,mimeDTO,true,null);
+	}
+	/**
+	 * 群发邮件 (附件)
+	 * @param userName
+	 * @param password
+	 * @param targetAddress 多个邮件发送地址，以,分隔
+	 * @param mimeDTO
+	 * @param filepath      文件本地绝对路径
+	 * @return
+	 */
+	public  static boolean sendGroupEmailByFile(String userName,String password,String targetAddress,
+			MimeMessageDTO mimeDTO,List<String> filepath){
+		return publicsendEmail(userName,password,targetAddress,mimeDTO,true,filepath);
+	}
+	
+	
+	
+	/**
+	 * 邮件发送基础方法
+	 * @param userName
+	 * @param password
+	 * @param targetAddress
+	 * @param mimeDTO
+	 * @param isGroup
+	 * @param filepath
+	 * @return
+	 */
+	private static boolean publicsendEmail(String userName,String password,String targetAddress,
+			MimeMessageDTO mimeDTO,boolean isGroup,List<String> filepath){
+		Properties props = makeMailProperties(userName);
 		String hostname=SMTPUtil.SimpleMailSender(userName);
-		System.out.println("当前邮箱host为:");
-		props.put("mail.smtp.host", hostname);
-		props.setProperty("mail.smtp.auth", "true");
-		props.setProperty("mail.transport.protocol", "smtp");
-		if(hostname.indexOf(".qq.com")!=-1){
-			props.setProperty("mail.smtp.socketFactory.port", "465");
-			props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		}
 		Session session = Session.getInstance(props, new PopupAuthenticator(userName, password));
-		session.setDebug(false);
+		session.setDebug(true);
 		try {
 			Transport ts = session.getTransport();
 			ts.connect(hostname,userName,password);
-			Message message = createEmail(session,userName,targetAddress,mimeDTO);
+			Message message =!isGroup?createEmail(session,userName,targetAddress,mimeDTO)
+					:createEmailByGroupAndFile(session,userName,
+							targetAddress,mimeDTO,filepath==null?null:filepath);
 			ts.sendMessage(message,message.getAllRecipients());
 			ts.close();
-			
-			
 		} catch (Exception mex) {
 			mex.printStackTrace();
 			return false;
@@ -77,13 +130,37 @@ public class MailUtil {
 		return true;
 	}
 	
+	
+	
+	
+	
+	/**
+	 * 创建邮件信息
+	 * @param userName
+	 * @return
+	 */
+	private static Properties makeMailProperties(String userName){
+		Properties props = new Properties();
+		String hostname=SMTPUtil.SimpleMailSender(userName);
+		props.put("mail.smtp.host", hostname);
+		props.setProperty("mail.smtp.auth", "true");
+		props.setProperty("mail.transport.protocol", "smtp");
+		if(hostname.indexOf(".qq.com")!=-1){
+			props.setProperty("mail.smtp.socketFactory.port", "465");
+			props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		}
+		return props;
+	}
+	
+	
+	
 	/**
 	 * 创建邮件
 	 * @Author Casper
 	 * Create_time:2015年10月17日 下午7:45:57
 	 * description:
 	 */
-	public static Message createEmail(Session session,String userName,String regMail,MimeMessageDTO mimeDTO){
+	private static Message createEmail(Session session,String userName,String regMail,MimeMessageDTO mimeDTO){
 		MimeMessage message = new MimeMessage(session);
 		try {
 			message.setFrom(new InternetAddress(userName));
@@ -96,6 +173,62 @@ public class MailUtil {
 		}
 		return message;
 	}
+	/**
+	 * 创建群发带附件
+	 * @return
+	 */
+	private static Message createEmailByGroupAndFile(Session session,String userName,
+								String regMail,MimeMessageDTO mimeDTO,List<String> filepath){
+		MimeMessage message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress(userName));
+			  // 创建收件人列表  
+	        if (regMail != null && regMail.trim().length() > 0) {  
+	            String[] arr = regMail.split(",");  
+	            int receiverCount = arr.length; 
+	            if (receiverCount > 0) {
+	            	InternetAddress[] address = new InternetAddress[receiverCount];  
+	                for (int i = 0; i < receiverCount; i++) {  
+	                    address[i] = new InternetAddress(arr[i]);  
+	                }  
+	                message.setRecipients(Message.RecipientType.TO, address);
+	            }
+	        }
+	     // 后面的BodyPart将加入到此处创建的Multipart中  
+            Multipart mp = new MimeMultipart();  
+	     // 附件操作  
+            if (filepath != null && filepath.size() > 0) {  
+                for (String filename : filepath) {  
+                    MimeBodyPart mbp = new MimeBodyPart();  
+                    // 得到数据源  
+                    FileDataSource fds = new FileDataSource(filename);  
+                    // 得到附件本身并至入BodyPart  
+                    mbp.setDataHandler(new DataHandler(fds));  
+                    // 得到文件名同样至入BodyPart  
+                    mbp.setFileName(fds.getName());  
+                    mp.addBodyPart(mbp);  
+                }  
+                MimeBodyPart mbp = new MimeBodyPart();  
+                mbp.setText(mimeDTO.getText());  
+                mp.addBodyPart(mbp);  
+                // 移走集合中的所有元素  
+                filepath.clear();  
+                // Multipart加入到信件  
+                message.setContent(mp);  
+            } else {  
+                // 设置邮件正文  
+//            	message.setText(mimeDTO.getText());  
+            	message.setContent(mimeDTO.getText(),"text/html;charset=UTF-8");
+            } 
+			message.setSubject(mimeDTO.getSubject());
+			message.saveChanges();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		return message;
+	}
+	
+	
 	
 	public String getUserName() {
 		return userName;
--- easymail/src/test/cn/gavinnie/test/mailTest.java
+++ easymail/src/test/cn/gavinnie/test/mailTest.java
@@ -1,6 +1,8 @@
 package test.cn.gavinnie.test;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,30 +13,52 @@ public class mailTest {
 
 	@Test
 	public void testSendEmail() {
-		
-//		//测试163邮箱
-		//测试qq邮箱 
-		//新浪邮箱 13077403326m@sina.cn
-		//139邮箱 13077403326@139.com
-		
-		
-		String userName = "****@139.com";   //用户邮箱地址
-		String password = "*****";    //密码或者授权码
-		String targetAddress = "****@139.com";	 //接受者邮箱地址
-		
-		//设置邮件内容
+
+		// //测试163邮箱
+		// 测试qq邮箱
+		// 新浪邮箱 13077403326m@sina.cn
+		// 139邮箱 13077403326@139.com
+
+		String userName = "13927734916@163.com"; // 用户邮箱地址
+		String password = "wfl970530"; // 密码或者授权码
+		String targetAddress = "791120662@qq.com"; // 接受者邮箱地址
+
+		// 设置邮件内容
 		MimeMessageDTO mimeDTO = new MimeMessageDTO();
 		mimeDTO.setSentDate(new Date());
 		mimeDTO.setSubject("邮件的标题");
-		mimeDTO.setText("邮件的内容"+targetAddress);
-		
-		//发送邮件
-		if(MailUtil.sendEmail(userName, password, targetAddress, mimeDTO)){
+		mimeDTO.setText("邮件的内容<img src='http://static.oschina.net/uploads/user/111/223750_100.jpg'>"
+				+ targetAddress);
+
+//		// 发送单邮件
+//		if (MailUtil.sendEmail(userName, password, targetAddress, mimeDTO)) {
+//			System.out.println("邮件发送成功！");
+//		} else {
+//			System.out.println("邮件发送失败!!!");
+//		}
+		// 发送单邮件(附件)
+		List<String> filepath=new ArrayList<String>();
+		filepath.add("D:/temple.xls");
+		filepath.add("D:/test.xls");
+//		if (MailUtil.sendEmailByFile(userName, password, targetAddress, mimeDTO,filepath)) {
+//			System.out.println("邮件发送成功！");
+//		} else {
+//			System.out.println("邮件发送失败!!!");
+//		}
+		// 群发邮件
+		targetAddress = "791120662@qq.com,1466209469@qq.com";
+//		if (MailUtil.sendGroupEmail(userName, password, targetAddress, mimeDTO)) {
+//			System.out.println("邮件发送成功！");
+//		} else {
+//			System.out.println("邮件发送失败!!!");
+//		}
+//		// 群发邮件(附件)
+		if (MailUtil.sendGroupEmailByFile(userName, password, targetAddress, mimeDTO,filepath)) {
 			System.out.println("邮件发送成功！");
-		}else{
+		} else {
 			System.out.println("邮件发送失败!!!");
 		}
-		
+
 	}
 
 }
